99b6560329a53b5bc0ea8664916e92e7773d351f
AbdulAfeez <abdulafeez.adeyemo@kibo.school>
2024-05-06 15:51:18 +0100

    feat: Added Cors to allow applications access the API

--- MayorBlings/MayorBlings/settings.py
+++ MayorBlings/MayorBlings/settings.py
@@ -48,9 +48,11 @@ INSTALLED_APPS = [
     'Authentication',
     'Product',
     'Admin',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -58,6 +60,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://localhost:5173",
 ]
 
 ROOT_URLCONF = 'MayorBlings.urls'
