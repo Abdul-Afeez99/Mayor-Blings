ce3df99378fb4a60773807c59f3870fa81dbd774
AbdulAfeez Adeyemo <114268023+Abdul-Afeez99@users.noreply.github.com>
2024-07-09 01:30:21 +0100

    Update models.py

--- MayorBlings/Product/models.py
+++ MayorBlings/Product/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Categories(models.Model):
-    category_name = models.CharField(max_length=50)
+    category_name = models.CharField(max_length=200)
     category_description = models.TextField()
     category_image = models.URLField()
     created_at = models.DateTimeField(auto_now_add=True)
@@ -14,11 +14,11 @@ class Categories(models.Model):
     
 class Products(models.Model):
     category = models.ForeignKey(Categories, on_delete=models.CASCADE)
-    product_name = models.CharField(max_length=50)
+    product_name = models.CharField(max_length=200)
     product_description = models.TextField()
     product_image = models.URLField()
     product_quantity = models.IntegerField()
-    product_price = models.DecimalField(max_digits=10, decimal_places=2)
+    product_price = models.DecimalField(max_digits=15, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
